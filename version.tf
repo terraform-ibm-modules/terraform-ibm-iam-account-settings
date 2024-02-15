1d5219e2795a979edd3909e431397fbb857e36cc
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2024-02-15 05:44:46 -0500

    fix: add extra variable validation to the api_endpoint variable in the experimental submodule (#256)

--- version.tf
+++ version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.3.0, <1.6.0"
   required_providers {
-    # Use "greater than or equal to" range in modules
+    # Each required provider's version should be a flexible range to future proof the module's usage with upcoming minor and patch versions.
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = ">= 1.56.1, < 2.0.0"
