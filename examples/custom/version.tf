b094ce43ec4a15ffbbbd8c7ffbc245ba6ea151a2
Muhammad Arif <CC108-muhammad-arif@CC108-Muhammad-Arif.local>
2024-02-26 03:29:03 +0500

    terraform example code optimized

--- examples/custom/version.tf
+++ examples/custom/version.tf
@@ -1,7 +1,8 @@
 terraform {
   required_version = ">= 1.0.0, <1.6.0"
   required_providers {
-    # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
+    # Ensure that there is always 1 example locked into the lowest provider version of the range defined in the main
+    # module's version.tf (basic example), and 1 example that will always use the latest provider version (advanced example).
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = ">= 1.49.0, < 2.0.0"
