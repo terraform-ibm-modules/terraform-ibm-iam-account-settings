9088c4983eb58466b0731f09f02a66d2a1f84b30
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-03-10 04:10:24 +0000

    fix(deps): update required_provider to latest for the fscloud deployable architecture (#269)

--- solutions/fscloud/version.tf
+++ solutions/fscloud/version.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.62.0"
+      version = "1.63.0"
     }
     restapi = {
       source  = "Mastercard/restapi"
-      version = "1.18.2"
+      version = "1.19.1"
     }
   }
 }
