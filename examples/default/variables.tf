4bd9d5c8dc306088f1c3241a77ec4a9c716263d6
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-11-22 15:00:54 +0000

    Update variables.tf

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -7,5 +7,5 @@ variable "ibmcloud_api_key" {
 variable "allowed_ip_addresses" {
   description = "List of the IP addresses and subnets from which IAM tokens can be created for the account."
   type        = list(any)
-  default     = ["0.0.0.0/0"]
+  default     = ["0.0.0.0/0", "::"]
 }
